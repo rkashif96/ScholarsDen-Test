82bfe639a5f6362d46a487e3b430e518789a22c6
Kashif <l174213@lhr.nu.edu.pk>
2023-12-26 07:51:08 +0500

    fixing environment issues

--- middleware.ts
+++ middleware.ts
@@ -10,13 +10,13 @@ const verify = (token: string, sk: string) => {
 const Response = (obj: any, status: any) => {
     return NextResponse.json({ obj, status })
 }
-export function middleware(request: NextRequest) {
+export function middleware(request: NextRequest, next: any) {
     try {
         const path = request.nextUrl.pathname
         const token = cookies().get("token")?.value || ''
         const decoded = verify(String(token), 'mySecretKey')
         if (!decoded && path != '/api/login' && path != '/api/register' && path != '/api/register') { return Response({ status: 400, message: 'User Not Logged In.' }, 400) }
-
+        next()
     } catch (error) {
         console.log(error)
     }
