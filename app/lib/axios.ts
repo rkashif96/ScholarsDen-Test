07dfd7871ec40d6b0b6209b87f5b88ac4940342f
Kashif <l174213@lhr.nu.edu.pk>
2023-12-26 07:36:32 +0500

    fixing environment issues

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_ENVIRONMENT == 'development' ? 'http://localhost:3000/api' : 'https://scholarden-test-eight.vercel.app/api/'
+const baseURL = process.env.NEXT_PUBLIC_ENVIRONMENT == 'development' ? 'http://localhost:3000/api' : 'https://scholarden-test-rust.vercel.app/api/'
 const instance = axios.create({ baseURL });
 
 export default instance;
